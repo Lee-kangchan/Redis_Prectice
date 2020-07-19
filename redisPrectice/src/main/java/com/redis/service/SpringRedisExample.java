fc7c65e4434e365a03d3a1102bf0055d56838ec2
Lee-kangchan <lsc4237@gmail.com>
2020-07-20 01:53:08 +0900

    Redis 값 예제

--- redisPrectice/src/main/java/com/redis/service/SpringRedisExample.java
+++ redisPrectice/src/main/java/com/redis/service/SpringRedisExample.java
@@ -1,13 +1,17 @@
 package com.redis.service;
 
+import java.time.Instant;
 import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.data.redis.core.HashOperations;
+import org.springframework.data.redis.core.ListOperations;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.data.redis.core.ZSetOperations;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.stereotype.Service;
 
 import com.redis.app.SpringRedisConfig;
@@ -15,11 +19,14 @@ import com.redis.app.vo.Employee;
 
 @Service
 public class SpringRedisExample {
+	static int TIMES = 10;
 	public void exam() {
 		ConfigurableApplicationContext ctx = new AnnotationConfigApplicationContext(SpringRedisConfig.class);
 		try {
 			@SuppressWarnings("unchecked")
 			RedisTemplate<String, Object> redisTemplate = (RedisTemplate<String, Object>)ctx.getBean("redisTemplate");
+
+			redisTemplate.setKeySerializer(new StringRedisSerializer());
 			// value operation
 			ValueOperations<String, Object> values = redisTemplate.opsForValue();
 			
@@ -43,7 +50,9 @@ public class SpringRedisExample {
 		try {
 			@SuppressWarnings("unchecked")
 			RedisTemplate<String, Object> redisTemplate = (RedisTemplate<String, Object>)ctx.getBean("redisTemplate");
-			
+
+			redisTemplate.setKeySerializer(new StringRedisSerializer());
+			redisTemplate.setValueSerializer(new StringRedisSerializer());
 			Map<String, String> empBobMap = new HashMap<String, String>();
 			empBobMap.put("name", "Bob");
 			empBobMap.put("age", "26");
@@ -79,7 +88,9 @@ public class SpringRedisExample {
 		try {
 			@SuppressWarnings("unchecked")
 			RedisTemplate<String, Object> redisTemplate = (RedisTemplate<String, Object>)ctx.getBean("redisTemplate");
-			
+
+			redisTemplate.setKeySerializer(new StringRedisSerializer());
+			redisTemplate.setValueSerializer(new StringRedisSerializer());
 			Map<String, String> people = new HashMap<String, String>();
 			people.put("name", name);
 			people.put("age", age);
@@ -101,11 +112,54 @@ public class SpringRedisExample {
 		}
 		
 	}
-	public void list() {
-		
+	public void list(String name, String value) {
+		ConfigurableApplicationContext ctx = new AnnotationConfigApplicationContext(SpringRedisConfig.class);
+		try {
+			@SuppressWarnings("unchecked")
+			RedisTemplate<String, Object> redisTemplate = (RedisTemplate<String, Object>)ctx.getBean("redisTemplate");
+
+			redisTemplate.setKeySerializer(new StringRedisSerializer());
+			
+			ListOperations<String, Object> lists = redisTemplate.opsForList();
+			Instant instant = Instant.now();
+			long times = instant.toEpochMilli();
+			
+			lists.leftPush(name, value, times);
+			lists.trim(name, 0, 10);
+				
+			System.out.println(name+ ": " + lists.getOperations());
+			
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+		finally {
+			ctx.close();
+		}
 	}
-	public void sort() {
-		
+	public void sort(String name, String value) {
+		ConfigurableApplicationContext ctx = new AnnotationConfigApplicationContext(SpringRedisConfig.class);
+		try {
+			@SuppressWarnings("unchecked")
+			RedisTemplate<String, Object> redisTemplate = (RedisTemplate<String, Object>)ctx.getBean("redisTemplate");
+
+			redisTemplate.setKeySerializer(new StringRedisSerializer());
+			
+			ZSetOperations<String, Object> lists = redisTemplate.opsForZSet();
+			Instant instant = Instant.now();
+			long times = instant.toEpochMilli();
+			
+			lists.add(name, value, times);
+			lists.removeRange(name, -(TIMES-1), -(TIMES-1));	
+			System.out.println(name+ ": " + lists.toString());
+			
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+		finally {
+			ctx.close();
+		}
 	}
 	public void set() {
 		
